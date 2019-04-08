e090b21395917c55f4a87db7872e68a6e2596e70
Clark Joy Pesco <clarkjoypesco@yahoo.com>
2019-04-08 19:27:23 +0800

    initial commit

---
+++ capitalname.py
@@ -0,0 +1,14 @@
+# Write Python code that prints out Clark Joy (with a capital C and J), 
+# given the definition of z below.
+
+z = 'xclarkjoy'
+
+name1 = z[1:6]
+name1 = name1.upper()
+name1 = name1.title()
+name2 = z[-3:]
+name2 = name2.upper()
+name2 = name2.title()
+
+
+print  name1 + ' ' + name2
